6206d7f7cd6dab2f45b5b55025519deb966e906f
codeaches <info001@codeaches.com>
2019-12-17 06:52:50 -0700

    Spring Boot Kafka Examples [http://codeaches.com/]

---
+++ src/test/java/com/codeaches/kafka/basics/KafkaProducerConsumerBasicsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.codeaches.kafka.basics;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+
+@SpringBootTest
+@EmbeddedKafka
+class KafkaProducerConsumerBasicsApplicationTests {
+
+//  @Test
+  void contextLoads() {
+  }
+
+}
